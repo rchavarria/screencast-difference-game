0f9544387402ded8f949efd57923b14e1eda9fdd
rchavarria <txingo@gmail.com>
2014-09-23 00:12:54 +0200

    Refactor : extract 'test()' method

--- web/screencast.dart
+++ web/screencast.dart
@@ -3,13 +3,16 @@ import 'assertions.dart';
 
 void main() {
   try {
-    Assert assertThat = new Assert();
-    
-    assertThat.areEqual(4, 2 + 2, '4 must be equal to 2+2');
-    
-    stdout.writeln('The end!');
-    
+    test();
   } catch (e) {
     stderr.writeln(e.message);
   }
+}
+
+void test() {
+  Assert assertThat = new Assert();
+  
+  assertThat.areEqual(4, 2 + 2, '4 must be equal to 2+2');
+  
+  stdout.writeln('The end!');
 }
